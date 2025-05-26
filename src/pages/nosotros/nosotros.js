d8e2c1bc0fee36dfa1bb3a353798400e23e66da8
Rene Cruz <m144renecruz2041@gmail.com>
2025-05-26 11:42:07 -0600

    feat: correcion de codigo

--- src/pages/nosotros/nosotros.js
+++ src/pages/nosotros/nosotros.js
@@ -74,3 +74,20 @@ updateCarousel();
 
 /* --------------------------------- */
 
+ // Back to Top Button
+    const backToTopBtn = document.getElementById('back-to-top');
+    
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            backToTopBtn.style.display = 'flex';
+        } else {
+            backToTopBtn.style.display = 'none';
+        }
+    });
+    
+    backToTopBtn.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
