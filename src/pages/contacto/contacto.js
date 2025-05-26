8225b9563555165163426cfe40c82f629785281e
Rene Cruz <m144renecruz2041@gmail.com>
2025-05-26 17:30:22 -0600

    feat: se creo estructura basica de contacto

---
+++ src/pages/contacto/contacto.js
@@ -0,0 +1,6 @@
+ function submitForm() {
+            const nombre = document.getElementById('nombre').value;
+            const apellidos = document.getElementById('apellidos').value;
+            const email = document.getElementById('email').value;
+            const dudas = document.getElementById('dudas').value;
+        }
