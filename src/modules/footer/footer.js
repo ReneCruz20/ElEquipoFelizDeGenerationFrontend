0851acffee6484028a26ff7a5146532aefde6dd4
RamsesMyMz <odin.mymz@gmail.com>
2025-05-27 11:08:25 -0600

    feat: pages-contacto updated social icons and hyperlinks

--- src/modules/footer/footer.js
+++ src/modules/footer/footer.js
@@ -16,8 +16,8 @@ const insertFooter = (footer)=>{
               alt="Facebook" /></a>
           <a href="https://api.whatsapp.com/send/?phone=5215583851010&text&type=phone_number&app_absent=0"
             target="_blank"><img src="/resources/images/socials/whatsapp.svg" alt="WhatsApp" /></a>
-          <!--     <a href="#" target="_blank"><img src="/resources/images/socials/"
-              alt="socials" /></a>     -->
+          <a href="https://maps.app.goo.gl/tM23jumG1VvATGD48" target="_blank"><img src="/resources/images/socials/google-maps.svg"
+              alt="Gmaps" /></a>
         </div>
       </div>
       
