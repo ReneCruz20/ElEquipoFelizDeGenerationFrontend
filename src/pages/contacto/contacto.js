e2641861dbdd92a2cb5617685726d9364d23ad88
Perla Lopez <perlalopez35046@gmail.com>
2025-05-26 17:59:24 -0700

    feat: resolución de conflictos con el merge a la rama remota

--- src/pages/contacto/contacto.js
+++ src/pages/contacto/contacto.js
@@ -1,11 +1,11 @@
-
-
-import { insertFooter } from "../../modules/footer/footer.js";
-
-insertFooter(document.getElementById("footer"));
  function submitForm() {
             const nombre = document.getElementById('nombre').value;
             const apellidos = document.getElementById('apellidos').value;
             const email = document.getElementById('email').value;
             const dudas = document.getElementById('dudas').value;
         }
+
+
+import { insertFooter } from "../../modules/footer/footer.js";
+
+insertFooter(document.getElementById("footer"));
